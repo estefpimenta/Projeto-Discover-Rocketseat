e894e4dd2c1e9ad2a11b3245170963f653665df5
Estefanno Pimenta <estefanno.pimenta@gmail.com>
2024-01-31 11:44:37 -0300

    Personalizando o projeto

--- .vscode/script.js
+++ .vscode/script.js
@@ -1,18 +1,18 @@
-function toggleMode() {
+ function toggleMode() {
   const html = document.documentElement
     html.classList.toggle("light")
 
  // pegar a tag image
   const img = document.querySelector ("#profile img")
 
-  // substituir a imagem
+//  // substituir a imagem
 
   if (html.classList.contains ('light')) {
     // se tiver light mode, adicionar a imagem light
-    img.setAttribute ('src', './assets/avatar-light.png')
+    img.setAttribute ('src', './assets/Avatar_EP.png')
   } else {
      // se tiver sem light mode, manter a imagem normal
-     img.setAttribute("src", "./assets/avatar.png")
+     img.setAttribute("src", './assets/Avatar_EP.png')
   }
 
 
